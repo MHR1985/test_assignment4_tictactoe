d1d5d8e5a704fe729e68485d01c564668c3ede22
MHR1985 <cph-mr221@cphbusiness.dk>
2020-11-12 11:58:03 +0100

    added another end game condition

--- src/main/java/game/GameHandler.java
+++ src/main/java/game/GameHandler.java
@@ -17,7 +17,7 @@ public class GameHandler {
 
     public void runGame() throws BoardException {
         engine.initializeBoard();
-        while(!engine.checkForWin()){
+        while(!engine.checkForWin() || !engine.isBoardFull()){
             engine.printBoard();
             System.out.println("");
             System.out.println("It is player: " + engine.getCurrentPlayer());
@@ -31,11 +31,16 @@ public class GameHandler {
             System.out.println("");
 
         }
-        if(engine.getCurrentPlayer()=='X'){
-            System.out.println("PLAYER O WON!");
-        }else{
-            System.out.println("PLAYER X WON!");
+        if(engine.checkForWin()){
+            if(engine.getCurrentPlayer()=='X'){
+                System.out.println("PLAYER O WON!");
+            }else{
+                System.out.println("PLAYER X WON!");
+            }
+            engine.printBoard();
+        }else if(engine.isBoardFull()){
+            System.out.println("IT'S A DRAW");
         }
-        engine.printBoard();
+
     }
 }
